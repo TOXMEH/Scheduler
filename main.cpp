6cedfd242798e724533b75b3c451f7fd31665d93
TOXMEH <nesudimov.96@mail.ru>
2015-11-26 23:53:03 +1000

    Добавил сохранение списка дел в отдельном файле

--- main.cpp
+++ main.cpp
@@ -3,98 +3,112 @@
 #include <math.h>
 #include <cstddef>
 #include <iomanip>
-#include <stack>
 
 
 using namespace std;
 
 
 struct stack {
-   // struct stack *previous;
+    // struct stack *previous;
     string task;
     int priority;
     struct stack *next;
 };
 
+void Queue(stack *&st, string symbol) {
+    stack *top = new(stack);
+    top->task = symbol;
+    st->next = top;
+    st = top;
+}
 
 
-void Showtask(stack *& head){
-    stack * previous;
-    bool NotFirst=false;
+void Showtask(stack *&head, int &StackSize) {
+    stack *previous;
+    bool NotFirst = false;
 
-    while (head->priority > 1){
+    while (head->priority > 1) {
         previous = head;
-        head =previous->next;
-        NotFirst=true;
+        head = previous->next;
+        NotFirst = true;
     }
     cout << head->task << endl;
-    cout<<"Has the task been done?"<<endl;
+    cout << "Has the task been done?";
     string HasBeenDone;
-    cin>>HasBeenDone;
-    if ((HasBeenDone=="yes") && (NotFirst==false)){
-        previous->next= head->next;
+    cin >> HasBeenDone;
+    if ((HasBeenDone == "yes") && (NotFirst == false)) {
+        previous->next = head->next;
         delete head;
-    } else if (HasBeenDone=="yes"){
-        head=head->next;
+    } else if (HasBeenDone == "yes") {
+        head = head->next;
     }
 }
 
-void AddTask(stack *& tail){
+void AddTask(stack *&tail, int &StackSize) {
     cout << "Enter the task to add: ";
     string task;
-    cin>>task;
-    int priority=4;
-    while (priority>3 || (priority<1)) {
+    cin.sync();
+    getline(cin, task);
+    int priority = 4;
+    while (priority > 3 || (priority < 1)) {
         cout << "Enter its priority. It can have three ranges: ";
         cin >> priority;
     }
-    if (tail->task=="X"){
-        tail->task=task;
-        tail->priority=priority;
-    }else {
+    if (StackSize == 0) {
+        tail->task = task;
+        tail->priority = priority;
+        StackSize++;
+    } else {
         stack *next = new(stack);
         tail->next = next;
         next->task = task;
         next->priority = priority;
         tail = next;
+        StackSize++;
     }
 }
 
-void WriteToTheFile(stack *& head){
+void WriteToTheFile(stack *&head, int &StackSize) {
     ofstream output("tasks.txt");
-    while (head->task!="X"){
-        output << '\n';
-        output << head->task;
-        output.seekp(40);
-        output << head->priority;
-    }
+    while (StackSize > 0) {
+        output << head->priority << " " << head->task << '\n';
 
+        head = head->next;
+        StackSize--;
+    }
+}
+
+void ReadFromTheFile(stack *&head, int &StackSize) {
+    ifstream input("tasks.txt");
+    while (!(input.eof())){
+
+    }
 }
 
 
-int main(){
-    stack * tail;
-    stack * head=tail;
-    tail->task="X";
+int main() {
+    setlocale(LC_ALL, "russian");
+
+    int StackSize = 0;
+    stack *tail;
+    stack *head = tail;
+
     string answer;
     while (true) {
-        cout << "Enter 'show', if you want to see a task, or enter 'add', if you want to add a task: ";
+        cout <<
+        "Enter 's', if you want to see a task, or enter 'a', if you want to add a task, or enter '!', if you want to stop working with program: ";
 
         cin >> answer;
-        if (answer == "show") {
-            Showtask(head);
+        if (answer == "s") {
+            Showtask(head, StackSize);
         }
-        if (answer == "add") {
-            AddTask(tail);
+        if (answer == "a") {
+            AddTask(tail, StackSize);
         }
-        if (answer == "stop"){
+        if (answer == "!") {
             goto EXIT;
         }
     }
     EXIT:
-    //stack * next;
-    //next->task=*"X";
-    //tail->next=&next;
-    WriteToTheFile(head);
-    return 0;
+    WriteToTheFile(head, StackSize);
 }
