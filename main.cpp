0ce4dae6dc146fef62b53592d6aabc9470066e0b
TOXMEH <nesudimov.96@mail.ru>
2015-11-27 23:17:25 +1000

    Повышение приоиритета

--- main.cpp
+++ main.cpp
@@ -3,15 +3,37 @@
 #include <math.h>
 #include <cstddef>
 #include <iomanip>
+#include <time.h>
+#include <Windows.h>
 
 
 using namespace std;
+
+//русификация перегрузкой оператора вывода
+ostream& operator<<(ostream &stream, char *s){
+    for (char *ps=s; *ps; ps++){
+        if (*ps=='ё')
+            stream<<char(241);
+        else{
+            if (*ps =='Ё')
+                stream <<char(240);
+            else{
+                if (*ps<0)
+                    stream<<char(*ps+64+176);
+                else
+                    stream<<*ps;
+            }
+        }
+    }
+    return stream;
+}
 
 
 struct stack {
     // struct stack *previous;
     string task;
     int priority;
+
     struct stack *next;
 };
 
@@ -36,7 +58,7 @@ void Showtask(stack *&head, int &StackSize) {
     cout << "Has the task been done?";
     string HasBeenDone;
     cin >> HasBeenDone;
-    if ((HasBeenDone == "yes") && (NotFirst == false)) {
+    if ((HasBeenDone == "yes") && (NotFirst)) {
         previous->next = head->next;
         delete head;
     } else if (HasBeenDone == "yes") {
@@ -50,14 +72,15 @@ void AddTask(stack *&tail, int &StackSize) {
     cin.sync();
     getline(cin, task);
     int priority = 4;
-    while (priority > 3 || (priority < 1)) {
-        cout << "Enter its priority. It can have three ranges: ";
-        cin >> priority;
-    }
+
+    cout << "Enter its priority. It can have three ranges: ";
+    cin >> priority;
+
     if (StackSize == 0) {
         tail->task = task;
         tail->priority = priority;
         StackSize++;
+
     } else {
         stack *next = new(stack);
         tail->next = next;
@@ -65,23 +88,80 @@ void AddTask(stack *&tail, int &StackSize) {
         next->priority = priority;
         tail = next;
         StackSize++;
+
     }
+    cout << task << endl;
 }
 
 void WriteToTheFile(stack *&head, int &StackSize) {
-    ofstream output("tasks.txt");
-    while (StackSize > 0) {
-        output << head->priority << " " << head->task << '\n';
+    if (StackSize > 0) {
+        ofstream output("tasks.txt");
 
-        head = head->next;
-        StackSize--;
+        output << StackSize << '\n';
+        while (StackSize > 1) {
+            output << head->priority << " " << head->task << '\n';
+
+            head = head->next;
+            StackSize--;
+        }
+        output << head->priority << " " << head->task;
     }
 }
 
-void ReadFromTheFile(stack *&head, int &StackSize) {
-    ifstream input("tasks.txt");
-    while (!(input.eof())){
 
+void ReadFromTheFile(stack *&tail, int &StackSize) {
+    ifstream input("tasks.txt");
+    string inter;
+    getline(input, inter);
+    StackSize = stoi(inter);
+    //считывем первую строку, дабы не было пустых элементов в стеке
+    if (StackSize > 0) {
+        int priority;
+        input >> priority;
+        string task;
+
+        getline(input, task);
+        tail->task = task;
+        tail->priority = priority;
+    }
+    if (StackSize > 1) {
+        while (!(input.eof())) {
+            stack *next = new(stack);
+            tail->next = next;
+
+            int priority;
+            input >> priority;
+            next->priority = priority;
+
+            string task;
+            getline(input, task);
+            next->task = task;
+
+            tail = next;
+        }
+    }
+}
+
+void IncreasePriority(stack *&head, int StackSize) {
+    if (StackSize > 0) {
+        int size = StackSize;
+        stack *remembered = head;
+        while (size > 0) {
+            if (head->priority == 1) {
+                return;
+            }
+            head = head->next;
+            size--;
+        }
+
+        size = StackSize;
+        head = remembered;
+        while (size > 0) {
+            head->priority--;
+            head = head->next;
+            size--;
+        }
+        head = remembered;
     }
 }
 
@@ -92,6 +172,7 @@ int main() {
     int StackSize = 0;
     stack *tail;
     stack *head = tail;
+    ReadFromTheFile(tail, StackSize);
 
     string answer;
     while (true) {
@@ -99,10 +180,10 @@ int main() {
         "Enter 's', if you want to see a task, or enter 'a', if you want to add a task, or enter '!', if you want to stop working with program: ";
 
         cin >> answer;
-        if (answer == "s") {
+        if ((answer == "s") || (answer == "п")) {
             Showtask(head, StackSize);
         }
-        if (answer == "a") {
+        if ((answer == "a") || (answer == "д")) {
             AddTask(tail, StackSize);
         }
         if (answer == "!") {
@@ -110,5 +191,6 @@ int main() {
         }
     }
     EXIT:
+    IncreasePriority(head, StackSize);
     WriteToTheFile(head, StackSize);
 }
